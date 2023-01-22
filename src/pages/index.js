c59645a3729874af268a6802925a5e5c317bb21c
Minrey Peng <minreypeng@gmail.com>
2023-01-22 15:51:41 +0800

    index page done

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,14 @@ export default function Home() {
   return (
     <>
       <div className={styles.cover}></div>
+      <div className={styles.marquee}>
+        <div className={styles.marquee_content}>
+          MINIMALIST <span className={styles.decoDot}></span> WEB DEVELOPER <span className={styles.decoDot}></span> VISUAL DESIGNER<span className={styles.decoDot}></span>
+        </div>
+        <div className={styles.marquee_content}>
+          MINIMALIST <span className={styles.decoDot}></span> WEB DEVELOPER <span className={styles.decoDot}></span> VISUAL DESIGNER<span className={styles.decoDot}></span>
+        </div>
+      </div>
     </>
   )
 }
