6e10423ca9d951e327a92b5f13074765d556d300
Minrey Peng <minreypeng@gmail.com>
2023-02-27 09:03:43 +0800

    fixed letter case error

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-const layout = ({ children }) => {
+const Layout = ({ children }) => {
     const { route } = useRouter();
 
     return (
