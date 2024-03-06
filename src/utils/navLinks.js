eedd8299be769ab7168d20b7f62daba547e85bb4
MinreyP <minreypeng@gmail.com>
2024-03-06 15:03:11 +0800

    update index page image and style

--- src/utils/navLinks.js
+++ src/utils/navLinks.js
@@ -5,6 +5,11 @@ const navLinks = [
         titleTC: '首頁'
     },
     {
+        path: '/about',
+        titleEn: 'About',
+        titleTC: '關於'
+    },
+    {
         path: '/work',
         titleEn: 'Showcase',
         titleTC: '作品展示'
@@ -13,11 +18,6 @@ const navLinks = [
         path: 'https://github.com/MinreyP',
         titleEn: 'Github',
         titleTC: 'Github'
-    },
-    {
-        path: 'https://www.linkedin.com/in/minrey-peng/',
-        titleEn: 'LinkedIn',
-        titleTC: '個人檔案'
     },
     {
         path: 'mailto:minreypeng@gmail.com',
